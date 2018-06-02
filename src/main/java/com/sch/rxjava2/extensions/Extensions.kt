4fba480050c98c37735910959230e4898a93db36
sergey <sergey.chelombitko@gmail.com>
2018-06-02 16:39:54 +0300

    Add += operator for CompositeDisposable

--- src/main/java/com/sch/rxjava2/extensions/Extensions.kt
+++ src/main/java/com/sch/rxjava2/extensions/Extensions.kt
@@ -1,7 +1,15 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package com.sch.rxjava2.extensions
 
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 inline fun <reified T : Any> Observable<*>.ofType(): Observable<T> {
     return ofType(T::class.java)
+}
+
+inline operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
+    add(disposable)
 }
